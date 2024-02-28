44d13620a3cd8e7c219e05048bf530325eae3de3
DivyanshuAhirrao <246divyanahuahirrao@gmail.com>
2024-02-28 23:18:01 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import ColorChangeOnScrollGsap from "./components/ChangeColorOnScroll";
+import InfiniteScroll from "./components/InfiniteScroll";
+// import ScrollTriggerDirectionalMovement from "./components/ScrollTrigger";
+
+const App = () => {
+  return (
+    <>
+      {/* <ScrollTriggerDirectionalMovement /> */}
+      {/* <ColorChangeOnScrollGsap /> */}
+      <InfiniteScroll />
+    </>
+  );
+};
+
+export default App;
